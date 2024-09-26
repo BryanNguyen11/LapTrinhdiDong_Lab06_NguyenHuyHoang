07c6a292a87b2d5e63b6d9d106252bbbcd80a3f2
Huy Hoang <huyhoang.ng203@gmail.com>
2024-09-26 10:58:21 +0700

    Complete simple basic structure, not Plat list

--- Lab06/App.js
+++ Lab06/App.js
@@ -1,20 +1,165 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// In App.js in a new project
 
-export default function App() {
+import * as React from 'react';
+import { Button, View, Text, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Chats"
+        onPress={() => navigation.navigate('Chats')}
+      />
     </View>
   );
 }
+function Chats({Navigation}) {
+  return (
+    <View style={{ flex: 1, justifyContent: 'flex-start' }}>
+      <Text style={{textAlign:'center',fontSize:'30'}}>Bạn có thắc mắc về sản phẩm vừa xem. Đừng ngại chat với shop</Text>
+      
+      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
+        
+        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+        <View>
+
+        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
+        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
+
+        </View>
+
+        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
+          title="Chat"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+
+      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
+        
+        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
+
+        <View>
+
+        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
+        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
+
+        </View>
+
+        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
+          title="Chat"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+
+      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
+        
+        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
+
+        <View>
+
+        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
+        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
+
+        </View>
+
+        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
+          title="Chat"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+
+      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
+        
+        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
+
+        <View>
+
+        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
+        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
+
+        </View>
+
+        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
+          title="Chat"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+
+      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
+        
+        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
+
+        <View>
+
+        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
+        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
+
+        </View>
+
+        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
+          title="Chat"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+
+      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
+        
+        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
+
+        <View>
+
+        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
+        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
+
+        </View>
+
+        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
+          title="Chat"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+
+      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
+        
+        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
+
+        <View>
+
+        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
+        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
+
+        </View>
+
+        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
+          title="Chat"
+          onPress={() => navigation.navigate('Home')}
+        />
+      </View>
+    </View>
+
+    
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Chats" component={Chats} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+export default App;
---
+++ Lab06/screens/Details.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function DetailsScreen({Navigation}) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </View>
+    );
+  }
+  
+  const Stack = createNativeStackNavigator();
+  
+function App({Navigation}) {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Details" component={DetailsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
