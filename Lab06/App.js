dc332ec673ae7ca27fbfa7804bcdc7d8a5d1e3cb
Huy Hoang <huyhoang.ng203@gmail.com>
2024-10-02 21:48:37 +0700

    Change to flat list, Minh Chung

--- Lab06/App.js
+++ Lab06/App.js
@@ -1,16 +1,19 @@
 // In App.js in a new project
 
 import * as React from 'react';
-import { Button, View, Text, Image } from 'react-native';
+import { Button, View, Text, Image, FlatList, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-
+const products = [
+  { id: '1', name: 'Ca nấu lẩu', image: require('./assets/imgs/ca_nau_lau.png') },
+  // Thêm các sản phẩm khác vào đây
+];
 
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Home Screen</Text>
       <Button
         title="Go to Chats"
         onPress={() => navigation.navigate('Chats')}
@@ -18,132 +21,32 @@ function HomeScreen({ navigation }) {
     </View>
   );
 }
-function Chats({Navigation}) {
-  return (
-    <View style={{ flex: 1, justifyContent: 'flex-start' }}>
-      <Text style={{textAlign:'center',fontSize:'30'}}>Bạn có thắc mắc về sản phẩm vừa xem. Đừng ngại chat với shop</Text>
-      
-      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
-        
-        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
 
-        <View>
-
-        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
-        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
-
-        </View>
-
-        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
-          title="Chat"
-          onPress={() => navigation.navigate('Home')}
-        />
+function Chats({ navigation }) {
+  const renderItem = ({ item }) => (
+    <View style={styles.productContainer}>
+      <Image source={item.image} style={styles.productImage} />
+      <View>
+        <Text style={styles.productName}>Chat với shop</Text>
+        <Text style={styles.shopName}>Shop Devang</Text>
       </View>
-
-      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
-        
-        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
-
-        <View>
-
-        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
-        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
-
-        </View>
-
-        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
-          title="Chat"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-
-      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
-        
-        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
-
-        <View>
-
-        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
-        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
-
-        </View>
-
-        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
-          title="Chat"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-
-      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
-        
-        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
-
-        <View>
-
-        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
-        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
-
-        </View>
-
-        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
-          title="Chat"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-
-      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
-        
-        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
-
-        <View>
-
-        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
-        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
-
-        </View>
-
-        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
-          title="Chat"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-
-      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
-        
-        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
-
-        <View>
-
-        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
-        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
-
-        </View>
-
-        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
-          title="Chat"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
-
-      <View style={{flexDirection:'row', alignItems: 'center', justifyContent: 'space-around' }}  >
-        
-        <Image source={require('./assets/imgs/ca_nau_lau.png')} style={{width: 100, height: 100}} />
-
-        <View>
-
-        <Text style={{textAlign:'center',fontSize:'20'}}>Chat với shop</Text>
-        <Text style={{textAlign:'center',fontSize:'20', fontWeight:'bold',color:'red'}}>Shop Devang </Text>
-
-        </View>
-
-        <Button style={{textAlign:'center',fontSize:'20',backgroundColor:'red',MarginLeft:'10'}}
-          title="Chat"
-          onPress={() => navigation.navigate('Home')}
-        />
-      </View>
+      <Button
+        title="Chat"
+        color='red'
+        onPress={() => navigation.navigate('Home')}
+      />
     </View>
+  );
 
-    
+  return (
+    <View style={styles.container}>
+      <Text style={styles.chatText}>Bạn có thắc mắc về sản phẩm vừa xem. Đừng ngại chat với shop</Text>
+      <FlatList
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+    </View>
   );
 }
 
@@ -160,6 +63,42 @@ function App() {
   );
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginVertical: 16,
+  },
+  chatText: {
+    textAlign: 'center',
+    fontSize: 20,
+    marginVertical: 16,
+  },
+  productContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginVertical: 8,
+  },
+  productImage: {
+    width: 100,
+    height: 100,
+  },
+  productName: {
+    textAlign: 'center',
+    fontSize: 20,
+  },
+  shopName: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+});
 
 export default App;
